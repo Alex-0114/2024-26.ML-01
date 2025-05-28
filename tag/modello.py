22259fda3c6e70210d5b425649f41f16ccddb18b
Alessandro Tag <alessandro.tag@itsrizzoli.it>
2025-05-28 16:30:38 +0200

    creazione e compilazione modello.py

---
+++ tag/modello.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import plotly.express as px
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+
+df = pd.read_csv("C:\\Users\\AlessandroTag\\OneDrive - ITS Angelo Rizzoli\\Documenti\\2024-26.ML-01\\tag\\csv\\Salary Data.csv")
+df.dropna(axis=0, inplace=True)
+
+x = df["Years of Experience"]
+y = df["Salary"]
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+
+linear_regressor = LinearRegression()
+
+#Addestramento e predizione
+linear_regressor.fit(x_train.to_frame(), y_train)
+y_test_pred = linear_regressor.predict(x_test.to_frame())
+
+#Risultati
+print(mean_absolute_percentage_error(y_test, y_test_pred) * 100)
+
+# Predire i salari utilizzando il modello di regressione lineare
+df['Predicted Salary'] = linear_regressor.predict(df[['Years of Experience']])
+
+# Creare lo scatter plot con la retta di regressione
+fig = px.scatter(df, x="Years of Experience", y="Salary", title="Scatter Plot con Retta di Regressione")
+fig.add_scatter(x=df["Years of Experience"], y=df["Predicted Salary"], mode='lines', name='Retta di Regressione')
+fig.show()
